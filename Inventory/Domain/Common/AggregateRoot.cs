f8b79296b82ec624636a7d928c0df7f55698c6f4
Erik Báča <erik.baca@notino.com>
2024-10-07 19:39:14 +0200

    feat: Prepared DDD base objects

---
+++ Inventory/Domain/Common/AggregateRoot.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory.Domain.Common;
+
+internal abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+{
+    protected AggregateRoot() { }
+
+    protected AggregateRoot(TId id) : base(id) { }
+
+}
---
+++ Inventory/Domain/Common/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace Inventory.Domain.Common;
+
+internal abstract class Entity<TId> : IEquatable<Entity<TId>> where TId : notnull
+{
+    public TId Id { get; protected set; }
+
+    protected Entity() { }
+
+    protected Entity(TId id)
+    {
+        if (object.Equals(id, default(TId)))
+        {
+            throw new ArgumentException("The ID cannot be the default value.", "id");
+        }
+
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity<TId> entity && Id.Equals(entity.Id);
+    }
+
+    public static bool operator ==(Entity<TId> one, Entity<TId> two)
+    {
+        return Equals(one, two);
+    }
+
+    public static bool operator !=(Entity<TId> one, Entity<TId> two)
+    {
+        return !Equals(one, two);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public bool Equals(Entity<TId>? other)
+    {
+        return Equals((object?)other);
+    }
+}
